fe5f40917e50beb343b4596d1bfce87ec10f42a7
ZysiOK7 <166168143+ZysiOK7@users.noreply.github.com>
2024-04-16 10:54:32 +0200

    Add files via upload

---
+++ clothes_save.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nazwa = $_POST['nazwa'];
+    $wynik = $_POST['wynik'];
+    $czas = $_POST['czas'];
+
+    require('polaczenie.php');
+
+    if ($conn->connect_error) {
+        die("Błąd połączenia: " . $conn->connect_error);
+    }
+
+    $stmt = $conn->prepare("INSERT INTO clothes (nazwa, wynik, czas) VALUES (?, ?, ?)");
+    $stmt->bind_param("sis", $nazwa, $wynik, $czas);
+
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+
+}
+?>
