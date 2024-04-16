93f871f23737c0a7968ccc6aa330ff919a8a1824
13agatka13 <134784348+13agatka13@users.noreply.github.com>
2024-04-16 17:40:38 +0200

    Add files via upload

--- miejsca/js_miejsca.js
+++ miejsca/js_miejsca.js
@@ -524,7 +524,7 @@ function ShowScore() {
   }
 
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "../save_result.php", true);
+  xhr.open("POST", "../miejsca_save.php", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   // xhr.onreadystatechange = function () {};
   var data =
@@ -547,7 +547,7 @@ function handleNextButton() {
 }
 
 document.getElementById("see-results").addEventListener("click", function () {
-  window.location.href = "../wyniki_uczniow.php";
+  window.location.href = "wyniki_miejsca.php";
 });
 
 nextButton.addEventListener("click", () => {
